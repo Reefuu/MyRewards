69bddff4a7233489d01616203d533f377e213729
reefuu <rifqiet@gmail.com>
2023-05-15 09:03:20 +0700

    Initial Commit

---
+++ MyRewards/MyRewardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyRewardsApp.swift
+//  MyRewards
+//
+//  Created by Rifqie Tilqa Reamizard on 15/05/23.
+//
+
+import SwiftUI
+
+@main
+struct MyRewardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
