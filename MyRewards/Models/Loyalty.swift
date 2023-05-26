a4449fc348dab2968de7f5cb243305c84de16c39
mjwSolver <93109371+mjwSolver@users.noreply.github.com>
2023-05-26 19:10:43 +0700

    Created Loyalty Struct

--- MyRewards/Models/Loyalty.swift
+++ MyRewards/Models/Loyalty.swift
@@ -1,5 +1,5 @@
 //
-//  Store.swift
+//  Loyalty.swift
 //  MyRewards
 //
 //  Created by MacBook Pro on 27/05/23.
@@ -7,29 +7,33 @@
 
 import Foundation
 
-public struct Store: Codable {
+public struct Loyalty: Codable, Identifiable {
     public let id: Int
     public let store: String
     public let photo: String
+    public let points: Int
     public let foregroundColor: String
     public let backgroundColor: String
     public let createdAt: String
-
+    
     enum CodingKeys: String, CodingKey {
         case id = "id"
         case store = "store"
         case photo = "photo"
+        case points = "points"
         case foregroundColor = "foreground_color"
         case backgroundColor = "background_color"
         case createdAt = "created_at"
     }
-
-    public init(id: Int, store: String, photo: String, foregroundColor: String, backgroundColor: String, createdAt: String) {
+    
+    public init(id: Int, store: String, photo: String, points: Int, foregroundColor: String, backgroundColor: String, createdAt: String) {
         self.id = id
         self.store = store
         self.photo = photo
+        self.points = points
         self.foregroundColor = foregroundColor
         self.backgroundColor = backgroundColor
         self.createdAt = createdAt
     }
 }
+
