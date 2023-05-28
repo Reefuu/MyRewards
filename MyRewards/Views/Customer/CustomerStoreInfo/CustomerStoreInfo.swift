8523c0dbb12ea312299cb5b62c3eb453c679e18e
reefuu <rifqiet@gmail.com>
2023-05-29 06:52:40 +0700

    CustomerStoreInfo done, added AppEnvironment for it aswell

---
+++ MyRewards/Views/Customer/CustomerStoreInfo/CustomerStoreInfo.swift
@@ -0,0 +1,90 @@
+//
+//  CustomerStoreInfo.swift
+//  MyRewards
+//
+//  Created by Macbook Pro on 27/05/23.
+//
+
+import SwiftUI
+
+struct CustomerStoreInfo: View {
+    @EnvironmentObject var appEnv: AppEnvironment
+    @StateObject var viewModel: ViewModel = ViewModel()
+    
+    
+    var body: some View {
+        
+        ScrollView {
+            VStack(spacing: 20) {
+                
+                VStack(alignment:.leading) {
+                    
+                    HStack {
+                        Spacer()
+                        AsyncImage(url: URL(string: appEnv.selectedLoyalty.photo)) { image in
+                            image.resizable()
+                        } placeholder: {
+                            ProgressView()
+                        }
+                        .frame(width: 64, height: 64)
+                        .cornerRadius(5)
+                    }
+                    
+                    Spacer()
+                    
+                    
+                    Text(appEnv.selectedLoyalty.store)
+                        .font(.system(size: 20, weight: .bold))
+                        .foregroundColor(Color(appEnv.selectedLoyalty.foregroundColor))
+                    Text("\(appEnv.selectedLoyalty.points) Points")
+                        .font(.system(size: 20, weight: .semibold))
+                        .foregroundColor(Color(appEnv.selectedLoyalty.foregroundColor))
+                }
+                .padding()
+                .frame(height: 200)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .background(Color(appEnv.selectedLoyalty.backgroundColor))
+                .cornerRadius(10)
+                
+                Text("Your coupons")
+                    .font(.system(size: 20, weight: .bold))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                
+                LazyVStack {
+                    ForEach(appEnv.currentCoupon) { coupon in
+                        HStack(spacing: 5) {
+                            Image(systemName: "tag.slash")
+                            Text(coupon.title)
+                            Spacer()
+                            
+                            if coupon.redeemedAt != nil {
+                                Text("Invalid")
+                                    .foregroundColor(.red)
+                            }
+                        }
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding()
+                        .background(Color.white)
+                        .cornerRadius(10)
+                        .onTapGesture {
+                            viewModel.selectedCoupon = coupon
+                            viewModel.openSheet.toggle()
+                        }
+                    }
+                }
+                
+                Spacer()
+            }
+            .padding()
+            .navigationTitle(appEnv.selectedLoyalty.store)
+            .frame(maxWidth: .infinity, alignment: .leading)
+        }
+        
+    }
+}
+
+struct CustomerStoreInfo_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomerStoreInfo().environmentObject(AppEnvironment())
+    }
+}
